5b3958633169fbedc3151734a347bf242c071758
sahilansari2005 <sahilansari2212@gmail.com>
2026-06-10 22:31:43 +0400

    remove Dockerfile, use nixpacks

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "=== Running migrations ==="
-python manage.py migrate --no-input
-echo "=== Collecting static files ==="
-python manage.py collectstatic --no-input
-echo "=== Starting gunicorn on port ${PORT:-8000} ==="
-python -m gunicorn config.wsgi --bind 0.0.0.0:${PORT:-8000} --workers 2 --log-level debug 2>&1
-echo "=== Gunicorn exited ==="
